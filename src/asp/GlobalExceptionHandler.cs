f1e64370916cd410315ff2db1d81bfbac70cf537
seungyongshim <seungyongshim@gmail.com>
2025-01-27 23:10:30 +0900

    전역 예외 처리기 추가 및 요청 처리 개선
    `GlobalExceptionHandler.cs` 파일에 전역 예외 처리기를 추가하여 예외 발생 시 로그를 기록하고, 클라이언트에게 500 상태 코드와 함께 예외 메시지를 반환하도록 구현했습니다.
    
    `Program.cs` 파일에서는 예외 처리기를 등록하고, 잘못된 요청에 대해 예외를 발생시키도록 설정했습니다. 또한, `SampleDto` 레코드를 정의하여 JSON 요청의 구조를 명시했습니다.
    
    `asp.http` 파일에서는 GET 요청을 POST 요청으로 변경하고, 요청 본문에 잘못된 데이터 형식을 포함시켰습니다.

---
+++ src/asp/GlobalExceptionHandler.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+// 전역 예외 처리기 클래스
+public class GlobalExceptionHandler(ILogger<GlobalExceptionHandler> logger) : IExceptionHandler
+{
+    public async ValueTask<bool> TryHandleAsync(
+        HttpContext httpContext,
+        Exception exception,
+        CancellationToken cancellationToken)
+    {
+        logger.LogError(exception, "");
+
+        await Results.Problem(
+            detail: exception.Message,
+            statusCode: StatusCodes.Status500InternalServerError
+        ).ExecuteAsync(httpContext);
+
+        return true;
+    }
+}
--- src/asp/Program.cs
+++ src/asp/Program.cs
@@ -2,16 +2,31 @@ using System.Diagnostics;
 
 var builder = WebApplication.CreateBuilder(args);
 
+
+builder.Services.Configure<RouteHandlerOptions>(o => o.ThrowOnBadRequest = true);
+
+// 전역 예외 처리기 설정
+builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
 builder.Services.AddProblemDetails(options =>
 {
     options.CustomizeProblemDetails = ctx =>
     {
-       ctx.ProblemDetails.Extensions["traceId"] = Activity.Current?.Id ??   ;
+        ctx.ProblemDetails.Extensions["traceId"] = Activity.Current?.Id ?? ctx.HttpContext.TraceIdentifier;
     };
 });
 
 var app = builder.Build();
 
-app.MapPost("/", () => { }).WithName("GetWeatherForecast");
+// 예외 처리 미들웨어 활성화
+app.UseExceptionHandler();
+
+app.MapPost("/", (SampleDto dto) => { }).WithName("GetWeatherForecast");
 
 app.Run();
+
+
+record SampleDto
+{
+    public required string Name { get; init; }
+    public int Age { get; init; }
+}
